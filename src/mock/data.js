46780e9e0598a321980eadad9d358d383353bfe6
Egor18032019 <goro4@mail.ru>
2021-05-10 22:28:27 +0500

    Сделал модную визитку

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,27 +2,27 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'Serzh | Frontend', // e.g: 'Name | Developer'
+  title: 'Егор | JS developer', // e.g: 'Name | Developer'
   lang: 'ru', // e.g: en, es, fr, jp
-  description: 'frontend developer', // e.g: Welcome to my website
+  description: 'JavaScript developer', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: 'Serzhan',
-  subtitle: "I'm  frontend developer.",
-  cta: '',
+  title: 'Добрый день ! Меня зовут ',
+  name: 'Егор',
+  subtitle: 'Я Javasript разработчик',
+  cta: 'Ну или планирую им стать :-)',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'aboutme.webp',
   paragraphOne:
-    "Hello, I'm Serzhan, from Kazakhstan. I've been learning front-end development since last year. Very excited to learn, develop my skills and knowledge in this fast-changing web world :)",
-  paragraphTwo:
-    'Привет! Меня зовут Сержан, я из Казахстана. Изучаю фронтенд уже почти год. Всегда рад учится полезным навыкам и расширять знания в этом прогрессивном мире веба :)',
-  paragraphThree: ' ', // if no resume, the button will not show up
+    'Добрый день. Меня зовут Егор. Я начал изучать JavaSript разработку в 2019 году.И это меня увлекло.',
+  paragraphTwo: `Можно что еще написать.`,
+  paragraphThree: 'Ниже резюме с hh.',
+  resume: 'https://ekaterinburg.hh.ru/resume/dd37e7f8ff086e618e0039ed1f323635306275',
 };
 
 // PROJECTS DATA
@@ -30,39 +30,50 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project-1.png',
-    title: 'Player app',
-    info: 'Here you can search & play your favorite music, sample etc.',
-    info2: 'Technologies: React, Next, Mobx, tailwindcss',
-    url: 'https://meere.vercel.app',
-    repo: 'https://github.com/serzhan181/Meere', // if no repo, the button will not show up
+    title: 'workplace planner',
+    info: 'Рассадка сотрудников в офисе',
+    info2: 'Technologies: React, Redux, Webpack и немного TS',
+    url: 'https://blissful-torvalds-09138c.netlify.app/',
+    repo: 'https://github.com/Egor18032019/PlaningWorkSpaceOnOffice', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project-2.png',
-    title: 'Movie app',
+    title: 'Yandexmap app',
+    info: 'Одностроничный сайт где можно выбрать несколько маршрутов и в живую увидеть их',
+    info2: 'Technologies: React.',
+    url: 'https://goofy-swartz-e16c87.netlify.app/',
+    repo: 'https://github.com/Egor18032019/TeachYandexApiMap', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project-4.png',
+    title: 'Game Cloun app',
     info:
-      'Here you can search for primary data about movies, see the ratings, create articles in article section, read and rate them and comment on them.',
-    info2: 'Technologies: React, react-router, mobx, express, typeorm, postgresql.',
-    url: 'https://movie-opal.vercel.app/',
-    repo: 'https://github.com/serzhan181/Movie', // if no repo, the button will not show up
+      'Игра про кубики.Только один уровень. И прошу прощения за долгую загрузку=бесплатный хостинг',
+    info2: 'Technologies: JS, Phaser, Express, MongoDB',
+    info3: 'Логин:test3@test.com Пароль:123',
+    url: 'https://purrfect-autumn-linseed.glitch.me/login',
+    repo: 'https://github.com/Egor18032019/game-bubble-on-js', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project-3.png',
-    title: 'Drawing app',
-    info:
-      'This is more like of a game app. You can draw stuff online with your friends, or just with people around the internet. Unfortunately I couldnt deploy this due to the websocket related problems. But I\'m still wanting to, so maybe I will if I figure out how.',
-    info2: 'Technologies: Vanilla JS, bulma.css, express',
-    url: 'https://github.com/serzhan181/draw_me_please/',
-    repo: 'https://github.com/serzhan181/draw_me_please/', // if no repo, the button will not show up  
-  }
+    title: 'Game Danger Bombs app',
+    info: 'Игра где нужно собрать звезды и не коснуться бобмы.',
+    info2: 'Technologies: JS, Phaser, Express, MongoDB',
+    info3: 'Логин:test3@test.com Пароль:123',
+    info4: 'И прошу прощения за долгую загрузку=бесплатный хостинг',
+    url: 'https://loud-horse-mackerel.glitch.me/',
+    repo: 'https://github.com/Egor18032019/game-bubble-on-js', // if no repo, the button will not show up
+  },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: 'serzhan181@outlook.com',
+  cta: 'Написать мне на эл.почту',
+  btn: 'почта',
+  email: 'goro4@mail.ru',
 };
 
 // FOOTER DATA
@@ -71,17 +82,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'vk',
-      url: 'https://vk.com/idarhipovskiy',
+      url: 'https://vk.com/egorviktorovichprishedko',
     },
     {
       id: nanoid(),
       name: 'telegram',
-      url: 'https://t.me/serbaevvv',
+      url: 'https://t.me/Egor_from_Ekaterinburg',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: 'https://github.com/serzhan181',
+      url: 'https://github.com/Egor18032019/',
     },
   ],
 };
